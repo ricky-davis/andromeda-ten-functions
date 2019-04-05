c9583eb880ea72cc1fc0b781831452364ecda2c1
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-05 14:47:50 -0500

    finished ten functions

--- solutions.js
+++ solutions.js
@@ -20,3 +20,33 @@
  * and
  * concat
  */
+function isTrue(input){
+    return input ===true;
+}
+function isFalse(input){
+    return input ===false;
+}
+function not(input){
+    return !input;
+}
+function addOne(input){
+    return parseFloat(input)+1;
+}
+function isEven(input){
+    return parseFloat(input) %2 ===0;
+}
+function isIdentical(a,b){
+    return a===b;
+}
+function isEqual(a,b){
+    return (a==b && a!==b);
+}
+function or(a,b){
+    return a||b;
+}
+function and(a,b){
+    return a&&b;
+}
+function concat(a,b){
+    return String(a)+String(b);
+}
